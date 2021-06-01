db2d53101a075cb3a51a0d425b9ca2b0279f12d0
Franko Stipanov <fstipanov@gmail.com>
2021-06-01 16:12:11 +0200

    modified app.js - esc working

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsShowModal.length; i++)
-  btnsShowModal[i].addEventListener('click', function () {
-    console.log('Button click');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsShowModal[i].addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
